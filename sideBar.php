489aa4330fdbc94522c7904daa869affc3974e66
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-26 22:33:14 +0100

    in sideBar.php change the name of each li + add a new li... in style.css : center the header, add a border black for ul, display inline block and change the height + wifth for the li

--- sideBar.php
+++ sideBar.php
@@ -6,17 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    <ul>
+    <ul id="sideBar">
+        <li>Table 0</li>
         <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
-        <li>Table 1</li>
+        <li>Table 2</li>
+        <li>Table 3</li>
+        <li>Table 4</li>
+        <li>Table 5</li>
+        <li>Table 6</li>
+        <li>Table 7</li>
+        <li>Table 8</li>
+        <li>Table 9</li>
+        <li>Random</li>
+
     </ul>
 </body>
 </html>
