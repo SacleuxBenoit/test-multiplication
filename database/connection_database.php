df547038f96a2e010f0c2818ce10bfd1462b62f5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-27 14:52:31 +0100

    add the connection to the database in database/connection_database.php

---
+++ database/connection_database.php
@@ -0,0 +1,12 @@
+<?php
+include('./pass.php');
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=multiplicationTable;charset=utf8', 'root', $_SESSION['pass']);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+?>
